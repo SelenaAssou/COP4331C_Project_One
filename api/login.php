0b0c7cd56b41db1295b1010781afda3e278d22e8
Kristoff Kounlavong <toffysk@gmail.com>
2018-09-06 16:20:55 -0400

    Update login.php code v.2

--- api/login.php
+++ api/login.php
@@ -4,7 +4,10 @@ require "supportFunctions.php";
 require "User.php";
 require "dbInfo.cfg.php";
 
-//foo
+//headers to be used for SQL queries to specific row headers (needed in variable format for php parsing)
+$usernameHeader = UserFields::USERNAME;
+$passwordHeader = UserFields::PASSWORD;
+$idHeader = UserFields::ID;
 
 // get and sanitize xhr data
 $xhrRequest = getXhrRequestInfo();
@@ -17,4 +20,43 @@ if (is_null($xhrRequest)){
     $requestUsername = $xhrRequest[UserFields::USERNAME];
     $requestPassword = $xhrRequest[UserFields::PASSWORD];
     $hashedPassword = hash('sha512', $requestPassword);
+    
+    $sqlConnection = new mysqli('localhost', dbinfo::$dbUser, dbInfo::$dbPass, dbInfo::$db);
+    if ($sqlConnection->connect_error){
+        error_log($sqlConnection->connect_error);
+        returnError(null);
+    }
+
+    // Log error if it occurs
+    mysqli_report(MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT);
+
+    // connection established, begin query and grab data
+    try {
+        /** @var mysqli_result $sqlResult */
+        $sqlQuery = "SELECT $idHeader FROM `users` WHERE $usernameHeader LIKE ? and $passwordHeader LIKE ?";
+        $sqlStmt = $sqlConnection->prepare($sqlQuery);
+        $sqlStmt->bind_param('ss', $requestUsername, $hashedPassword);
+        $sqlStmt->execute();
+        $sqlResult = $sqlStmt->get_result();
+
+        if (mysqli_num_rows($sqlResult) == 0) {
+            $returnArr = array("login not found" -> -1);
+        }
+        
+        $user = User::convertFromSQL($sqlResult);
+        else {
+            $returnArr = array($user->$id);
+        }
+
+        returnXhrRequestAsJson($returnArr);
+
+    } catch (Exception $exception) {
+        error_log("Exception occurred during Request handling:" . $exception->getMessage());
+        returnError(null);
+    }
+}
+
+function returnError($errorCode) {
+    returnXhrRequestAsJson(array("error occurred"));
+    exit();
 }
