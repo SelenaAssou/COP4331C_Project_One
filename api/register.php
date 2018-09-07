f4acb260d73c9a8674cdba228ac45441005a3b6d
davidjmaria <david.j.maria@ieee.org>
2018-09-07 12:19:39 -0700

    Fix issue where duplicate entries caused an error

--- api/register.php
+++ api/register.php
@@ -32,7 +32,7 @@ if (is_null($xhrRequest)){
     }
 
     //log any error that occurs
-    mysqli_report(MYSQLI_REPORT_ERROR|MYSQLI_REPORT_STRICT);
+    mysqli_report(MYSQLI_REPORT_ERROR);
 
     //connection established, begin query and grab the data
     try {
